cb85bc8993301e77c6897d67161e9a6e6e38698e
Vasily Ilin <vasilin97@gmail.com>
2022-11-17 15:28:39 -0800

    sbtm works, made animation of attracting to origin

--- src/fokker_planck_probability_flow.jl
+++ src/fokker_planck_probability_flow.jl
@@ -1,132 +1,161 @@
 # implementing the algorithm from "probability flow solution of the fokker-planck equation" 2022
 
-using Distributions, Flux
+using Distributions, Flux, LinearAlgebra, Plots
+using Zygote: gradient
+using Flux.Optimise: Adam
+using Flux: params, train!
 
-function sbtm(ρ₀, timesteps, b, D, n)
-    xs = sample_dist(ρ₀, n)
-    trajectories = zeros(n, 1+length(timesteps)) # trajectories[i,k+1] is particle i at time k
-    trajectories[:, 1] = xs
-    for (k, Δt) in enumerate(timesteps)
-        s = optimize(xs)
-        x = trajectories[i, k]
-        trajectories[i, k+1] = x + Δt * (b(x)) + D(x)*s(x)
+maybewrap(x) = x
+maybewrap(x::T) where {T <: Number} = T[x]
+
+function plot_s_1d(s, xs)
+  x_coordinates = minimum(xs) : (maximum(xs) - minimum(xs))/99 : maximum(xs)
+  s_values = vcat(s.(eachcol(reshape(x_coordinates, 1, 100)))...)
+  p = plot(x_coordinates, s_values)
+  scatter!(p, reshape(xs, size(xs, 2)), vcat(s.(eachcol(xs))...))
+end
+
+# divergence of vector field s at x
+divergence(s, x) = sum(gradient(x -> s(x)[i], x)[1][i] for i in 1:length(x))
+loss(s, xs) = sum(sum(s(x).^2) + 2.0*divergence(s, x) for x in xs)
+loss(s, xs :: Matrix) =  loss(s, eachcol(xs)) 
+
+function custom_train!(s, xs; optimiser = Adam(), num_steps = 25)
+  θ = params(s)
+  grads = gradient(() -> loss(s, xs), θ)
+  for _ in 1:num_steps
+    Flux.update!(optimiser, θ, grads)
+  end
+end
+
+propagate(x, t, Δt, b, D, s) = x + Δt * b(x, t) + D(x, t)*s(x)
+
+"""
+xs  : sample from initial probability distribution
+Δts : list of Δt's
+b   : Rᵈ → Rᵈ
+D   : Rᵈ → Rᵈˣᵈ
+n   : number of particles
+s   : NN to approximate score ∇log ρ
+"""
+function sbtm(xs, Δts, b, D, s)
+  trajectories = zeros(size(xs)..., 1+length(Δts)) # trajectories[:, i, k+1] is particle i at time k
+  trajectories[:, :, 1] = xs
+  sbtm!(trajectories, Δts, b, D, s)
+  trajectories
+end
+
+function sbtm!(trajectories, Δts, b, D, s)
+  t = zero(eltype(Δts))
+  for (k, Δt) in enumerate(Δts)
+    xs_k = trajectories[:, :, k]
+    custom_train!(s, xs_k)
+    for (i, x) in enumerate(eachcol(xs_k))
+      trajectories[:, i, k+1] = propagate(x, t, Δt, b, D, s)
     end
-    return trajectories
+    t += Δt
+  end
 end
 
-sample_dist(d :: Distribution, n) = rand(d, n)
-
-# ] activate Zygote
-using Zygote: gradient
-using Flux
-
-const D = 5
-s = Chain(
-    Dense(D => 10, σ),
-    Dense(10 => D))
-∇s(s, x) = sum((gradient(x -> s(x)[i], x))[1][i] for i in 1:D)
-loss_grad(x) = gradient(s -> ∇s(s, x), s)
-loss_grad(rand(5))
-
-
-
-
-x = [2, 1];
-
-y = [2, 0];
-
-gs = gradient(Flux.params(x, y)) do
-         f(x, y)
-       end
-
-gs[x]
-
-gs[y]
-
-
-
-W = rand(2, 5)
-b = rand(2)
-
-predict(x) = W*x .+ b
-
-function loss(x, y)
-  ŷ = predict(x)
-  sum((y .- ŷ).^2)
+# learning to animate
+function animate_2d(trajectories, fps = 5)
+  p = scatter(trajectories[1,:,1], trajectories[2,:,1], 
+  title = "time 0",
+  label = nothing, 
+  color=RGB(0.0, 0.0, 1.0), 
+  xlims = (-1.5, 1.5), ylims = (-1.5, 1.5));
+  anim = @animate for k ∈ axes(trajectories, 3)
+    λ = k/size(trajectories, 3)
+    red = λ > 0.5 ? 2. *(λ - 0.5) : 0.
+    green = 1. - abs(1. - 2. * λ)
+    blue = λ < 0.5 ? 2. * (0.5-λ) : 0.
+    scatter!(p, trajectories[1,:,k], trajectories[2,:,k], 
+    title = "time $k",
+    label = nothing, 
+    color = RGB(red, green, blue), 
+    xlims = (-1.5, 1.5), ylims = (-1.5, 1.5))
+  end
+  gif(anim, "anim_fps$fps.gif", fps = fps)
 end
 
-x, y = rand(5), rand(2) # Dummy data
-loss(x, y) # ~ 3
+# experimenting with all particles attracting to the origin
+function attractive_origin()
+  d = 2
+  # ρ₀ = MvNormal(I(d))
+  # n = 10
+  # xs = rand(ρ₀, n)
+  Δts = 0.01*ones(50)
+  b(x, t) = -x
+  D(x, t) = 0.1
+  s = Chain(
+    Dense(d => 50, relu),
+    Dense(50 => d))
+  xs = Float64[-1  0  1 -1  0  1 -1  0  1;
+        -1 -1 -1  0  0  0  1  1  1];
+  sbtm(xs, Δts, b, D, s)
+end
+trajectories = attractive_origin()
+animation = animate_2d(trajectories)
 
-gs = gradient(() -> loss(x, y), Flux.params(W, b, x))
-
-W -= 0.1*gs[W]
-b -= 0.1*gs[b]
-
-loss(x,y)
-
-
-
-W1 = rand(3, 5)
-b1 = rand(3)
-layer1(x) = W1 * x .+ b1
-
-W2 = rand(2, 3)
-b2 = rand(2)
-layer2(x) = W2 * x .+ b2
-
-model(x) = layer2(σ.(layer1(x)))
-
-model(rand(5)) # => 2-element vector
-
-  
-using Flux, Zygote, LinearAlgebra
-
-d = 5
-
-s = Chain(
-    Dense(d => 10, σ),
-    Dense(10 => d))
-
-∇s(x) = sum(diag(jacobian(s, x)[1])) # one way of getting divergence
-loss_grad(x) = gradient(() -> ∇s(x), Flux.params(s)) #errors
-loss_grad(rand(5)) # ERROR: Mutating arrays is not supported -- called copyto!(SubArray{Float64, 1, Matrix{Float64}, Tuple{Int64, Base.Slice{Base.OneTo{Int64}}}, true}, ...)
-
-∇s(x) = sum((gradient(x -> s(x)[i], x))[1][i] for i in 1:d) # another way of getting divergence
-loss_grad(x) = gradient(() -> ∇s(x), Flux.params(s)) #errors
-loss_grad(rand(5)) # ERROR: Can't differentiate foreigncall expression $(Expr(:foreigncall, :(:jl_eqtable_get), Any, svec(Any, Any, Any), 0, :(:ccall), %5, %3, %4)).
+# reproducing first numerical experiment: repelling particles attracted to a moving trap
+function repel_attract()
+  d_bar = 2
+  N = 50
+  d = d_bar*N
+  a = 2.
+  w = 1.
+  β(t) = a*[cos(π*w*t), sin(π*w*t)]'
 
 
-# loss(xs) = sum([sum(s(x).^2) + 2*∇s(x) for x in xs])
+
+
+end
 
 
 
 
 
-using Zygote: gradient
-g(x,y) = x+y 
-dxg(x,y) = gradient(x -> g(x,y), x)[1] #partial wrt x
-dxyg(x,y) = gradient(y -> dxg(x,y), y)[1] #mixed second derivative
-dxyg(1,1) # 0.0, as expected
 
-g1(x,y) = (x+y)[1]
-dxg1(x,y) = gradient(x -> g1(x,y), x)[1][1] #partial of g₁ wrt x₁
-dxg1(ones(2), ones(2)) # 1.0, as expected
-dxyg1(x,y) = gradient(y -> dxg1(x,y), y)[1][1] #partial of dxg1 wrt y₁
-dxyg1(ones(2), ones(2)) # ERROR: Need an adjoint for constructor Zygote.OneElement{Float64, 1, Tuple{Int64}, Tuple{Base.OneTo{Int64}}}. Gradient is of type Zygote.OneElement{Float64, 1, Tuple{Int64}, Tuple{Base.OneTo{Int64}}}
 
-g2(x,y) = transpose(x)*y
-dxg2(x,y) = gradient(x -> g2(x,y), x)[1][1] 
-dxyg2(x,y) = gradient(y -> dxg2(x,y), y)[1][1] 
-dxyg2(ones(2), ones(2)) # 1.0, as expected
 
-g3(x,y) = sum(x.*y)
-dxg3(x,y) = gradient(x -> g3(x,y), x)[1][1] 
-dxyg3(x,y) = gradient(y -> dxg3(x,y), y)[1][1] 
-dxyg3(ones(2),ones(2)) # 1.0, as expected
 
-g4(x,y) = x[1]*y[1] + x[2]*y[2]
-dxg4(x,y) = gradient(x -> g4(x,y), x)[1][1] #partial wrt x
-dxyg4(x,y) = gradient(y -> dxg4(x,y), y)[1][1] #mixed second derivative
-dxyg4(ones(2),ones(2)) # ERROR: Need an adjoint for constructor Zygote.OneElement{Float64, 1, Tuple{Int64}, Tuple{Base.OneTo{Int64}}}. Gradient is of type Vector{Float64}
+
+
+
+
+
+
+
+
+
+# learning the sin function
+function learn_sin()
+  s = Chain(
+    maybewrap,
+    Dense(1 => 50, relu),
+    Dense(50 => 50, relu),
+    Dense(50 => 50, relu),
+    Dense(50 => 50, relu),
+    Dense(50 => 1))
+  f(x) = sin.(6.28 .* x .+ 3.14)
+  x_train = hcat(0.5 : 3/100 : 3.5 ...)
+  data = [(x_train, f.(x_train))]
+  mse_loss(x, y) = Flux.Losses.mse(s(x), y)
+  p = plot(x_train[1,:], vcat(s.(x_train)[1,:]...));
+  for i in 1:5*10^3
+    train!(mse_loss, params(s), data, Descent(10^-1))
+    if i%10^3 == 0
+      @show mse_loss(data[1]...)
+      plot!(p, x_train[1,:], vcat(s.(x_train)[1,:]...))
+    end
+  end
+  s, p
+end
+
+# experimenting with (over)fitting on 3 points
+s, p = learn_sin()
+xs = [1. 2. 3.;] 
+custom_train!(s, xs, optimiser = Descent(10^-4), num_steps = 100)
+loss(s, xs)
+plot_s_1d(s,xs)
 
