10748fd6ccac15ae639f58be496a71e3799094d5
Vasily Ilin <vasilin97@gmail.com>
2022-12-01 12:44:43 -0800

    fixed figure

--- src/max.jl
+++ src/max.jl
@@ -1,4 +1,4 @@
-using Distributions, Plots
+using Distributions, Plots, QuadGK
 import StatsBase: moment
 
 pdf_M(dist, n, x) = n*cdf(dist, x)^(n-1)*pdf(dist, x)
@@ -21,25 +21,29 @@ function maxstats(dist, ns, m)
     end
     means, variances
 end
-function plot_max_stats(dist, ns, means, variances, dist_name)
-    p = plot(ns, means, label = "mean", title = "maximum of n $dist_name rv's", xaxis = "n", size = (1000,700));
-    plot!(ns, log.(ns), label = "expected mean: O(log n)");
+function plot_max_stats(dist, ns, means,  expected_means, variances, expected_variances, dist_name)
+    p = plot(ns, means, label = "Monte Carlo mean", title = "maximum of n $dist_name rv's", xaxis = "n", size = (1000,700), legend = :inside);
+    plot!(ns, expected_means[1], label = "expected mean: $(expected_means[2])");
     plot!(ns, mean_M.(dist, ns), label = "expected mean: numerical integration");
-    plot!(ns, variances, label = "variance");
-    plot!(ns, ones(size(ns)), label = "expected variance: O(1)");
+    plot!(ns, variances, label = "Monte Carlo variance");
+    plot!(ns, expected_variances[1], label = "expected variance: $(expected_variances[2])");
     plot!(ns, var_M.(dist, ns), label = "expected variance: numerical integration");
     # savefig(p, "max_$(dist_name)_stats")
     p
 end
-m = 10^3
+
+m = 3*10^2
 ns = 10:10^3:10^5
+means_exp, variances_exp = maxstats(Exponential(), ns, m)
+means_norm, variances_norm = maxstats(Normal(), ns, m)
 
-dist = Exponential()
-means_exp, variances_exp = maxstats(dist, ns, m)
-p_exp = plot_max_stats(Exponential(), ns, means_exp, variances_exp, "Exponential");
+expected_means_exp = (log.(ns) .+ MathConstants.γ, "log n + γ")
+expected_variances_exp = (π^2/6 .* ones(size(ns)), "π²/6")
+expected_means_norm = (.√(2 .* log.(ns)), "√(2log n)")
+expected_variances_norm = (1 ./ .√(log.(ns)), "1/√log n")
 
-dist = Normal()
-means_norm, variances_norm = maxstats(dist, ns, m)
-p_norm = plot_max_stats(Normal(), ns, means_norm, variances_norm, "Normal");
+p_exp = plot_max_stats(Exponential(), ns, means_exp, expected_means_exp, variances_exp, expected_variances_exp, "Exponential");
+
+p_norm = plot_max_stats(Normal(), ns, means_norm, expected_means_norm, variances_norm, expected_variances_norm, "Normal");
 p = plot(p_exp, p_norm)
 savefig(p, "max_stats_norm_exp")
